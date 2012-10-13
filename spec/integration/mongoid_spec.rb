45fa831537b0003b15bf352a31214626883afc0c
Julien 'Lta' BALLET <elthariel@gmail.com>
2012-10-14 01:25:50 +0200

    mongoid spec boilerplate

---
+++ spec/integration/mongoid_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+require 'mongoid'
+require 'rocket_pants/mongoid'
+
+describe RocketPants::Base, 'mongoid integration', :integration => true, :target => 'mongoid' do
+  pending "testing mongoid integration"
+end
+
